ec59d19fa926fda6a28bdf438c151f6ad47f73be
Hoàng Sơn <vuhson30799@gmail.com>
2019-07-21 08:45:24 +0700

    add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    AuthComponent,
+    HomeComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
